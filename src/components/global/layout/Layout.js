e362f561ddb11f73ff3dc5241706273698fa72e2
Shuhei Shagawa <sgw.sgw.shu@gmail.com>
2023-03-17 01:25:58 -0700

    setup slider nav

--- src/components/global/layout/Layout.js
+++ src/components/global/layout/Layout.js
@@ -3,19 +3,29 @@ import React, {useState, useEffect} from "react";
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
 
+import Sidebar from "react-sidebar";
+
 const Layout = ({children, langInfo}) => {
+  const [navStatus, toggleNav] = useState(false);
+
   return (
-    <>
+    <Sidebar
+      sidebar={<b>Sidebar content</b>}
+      open={navStatus}
+      onSetOpen={toggleNav}
+    >
       <div className="flex flex-col min-h-screen font-sans text-gray-900">
         <Header langInfo={langInfo} />
-
+        <div class="fixed" onClick={() => toggleNav(true)}>
+          Open sidebar
+        </div>
         <main className="flex-1 w-full max-w-4xl px-4 py-8 mx-auto md:px-8 md:py-16">
           {children}
         </main>
 
         <Footer />
       </div>
-    </>
+    </Sidebar>
   );
 };
 
