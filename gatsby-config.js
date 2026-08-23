346d5409d80fc686782ef1afea1ca8ca26b0be0e
shuhei <sgw.sgw.shu@gmail.com>
2026-08-24 00:20:29 +0900

    【修正】gatsby-plugin-dark-modeを消去

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,6 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    "gatsby-plugin-dark-mode",
     "gatsby-plugin-postcss",
     {
       resolve: `gatsby-source-wordpress`,
